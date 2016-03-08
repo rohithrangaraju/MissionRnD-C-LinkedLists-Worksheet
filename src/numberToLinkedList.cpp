008c7f09d4e6a31c8eb05640ca4fe550e91faf4a
rohithrangaraju <rohithrangaraju@missionrnd.com>
2016-03-08 21:58:42 +0530

    Submit Commit

--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -18,7 +18,38 @@ struct node {
 	int num;
 	struct node *next;
 };
-
+void insert(int n);
+struct node* head;
 struct node * numberToLinkedList(int N) {
-	return NULL;
+	int t;
+	head = NULL;
+	if (N > 0){
+		
+		while (N != 0){
+			t = N % 10;
+		    insert(t);
+			N = N / 10;
+		}
+		return head;
+	}
+	else if (N < 0){
+		N = N * (-1);
+		//head = NULL;
+		while (N != 0){
+			t = N % 10;
+			insert(t);
+			N = N / 10;
+		}
+		return head;
+	}
+	else if (N == 0){
+		insert(0);
+		return head;
+	}
+}
+void insert(int n){
+	node* temp = (node*)malloc(sizeof(node));
+	temp->num = n;
+	temp->next = head;
+	head = temp;
 }
